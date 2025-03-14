fc26d0945083722eff8369636f109962a8757829
simonjiao <simonjiaoh@gmail.com>
2025-03-15 00:39:15 +0800

    finish 03_hangman_game

--- mini-projects/03_hangman_game.py
+++ mini-projects/03_hangman_game.py
@@ -1,6 +1,7 @@
 # Description: A simple hangman game
 
 import random
+from collections import Counter
 
 some_words = """apple papaya banana orange mango strawberry pineapple
 grapefruit watermelon blueberry raspberry blackberry
@@ -16,29 +17,52 @@ if __name__ == "__main__":
         print("_", end=" ")
     print()
 
-    playing = True
     letters_guessed = ""
     chances = len(word) + 2
-    flag = 0
+    flag = 0  # indicates if the player has guessed the word
 
-    while chances != 0 and flag == 0:
-        chances -= 1
+    try:
+        while chances != 0 and flag == 0:
+            chances -= 1
 
-        guess = input("Guess a letter: ")
+            try:
+                guess = str(input("\nGuess a letter: "))
+            except ValueError:
+                print("Please enter only a letter.")
+                continue
 
-        # if letter is guessed correctly
-        if guess in word:
-            letters_guessed += guess
+            if not guess.isalpha():
+                print("Invalid input! Please enter a letter.")
+                continue
+            elif len(guess) != 1:
+                print("Invalid input! Please enter a single letter.")
+                continue
+            elif guess in letters_guessed:
+                print("You already guessed this letter.")
+                continue
 
-        flag = 1
-        for ch in word:
-            if ch in letters_guessed:
-                print(ch, end=" ")
-            else:
-                flag = 0
-                print("_", end=" ")
+            # if letter is guessed correctly
+            if guess in word:
+                k = word.count(guess)
+                for _ in range(k):
+                    letters_guessed += guess
 
-    if flag == 1:
-        print("\nYou won!")
-    elif chances <= 0:
-        print("\nYou lost! The word was:", word)
+            for ch in word:
+                if ch in letters_guessed and (
+                    Counter(letters_guessed) != Counter(word)
+                ):
+                    print(ch, end=" ")
+                # check if the word is guessed
+                elif Counter(letters_guessed) == Counter(word):
+                    print("\nYou won! The word is: ", word)
+                    flag = 1
+                    break
+                else:
+                    print("_", end=" ")
+
+        if chances <= 0 and flag == 0:
+            print("\nYou lost! The word was:", word)
+
+    except KeyboardInterrupt:
+        print("\nBye! See you next time.")
+        exit()
